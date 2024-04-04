075147622dcd0debda94fe0323bf5bac54bfa94a
tb0902 <tbruton417@gmail.com>
2024-04-04 14:38:10 -0400

    done, just need to make the last couple of tests

--- src/main/java/rocks/zipcode/io/ItemSorter.java
+++ src/main/java/rocks/zipcode/io/ItemSorter.java
@@ -1,15 +1,26 @@
 package rocks.zipcode.io;
 
+import rocks.zipcode.io.comparators.IdComparator;
+import rocks.zipcode.io.comparators.NameComparator;
+import rocks.zipcode.io.comparators.PriceComparator;
+
+import java.util.Arrays;
 import java.util.Comparator;
 
 /**
  * @author leon on 30/01/2019.
  */
 public class ItemSorter {
+    Item[] items;
+    IdComparator idComparator;
+    NameComparator nameComparator;
+    PriceComparator priceComparator;
     public ItemSorter(Item[] items) {
+        this.items = items;
     }
 
     public Item[] sort(Comparator<Item> comparator) {
-        return null;
+        Arrays.sort(items, comparator);
+        return items;
     }
 }
