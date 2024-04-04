acb643f4590438bc9b436332d6f52c07295cb991
tb0902 <tbruton417@gmail.com>
2024-04-04 13:43:36 -0400

    items tests pass :P

--- src/main/java/rocks/zipcode/io/Item.java
+++ src/main/java/rocks/zipcode/io/Item.java
@@ -4,30 +4,44 @@ package rocks.zipcode.io;
  * @author leon on 30/01/2019.
  */
 public class Item {
+    String name;
+    Double price;
+    Long id;
+
     public Item() {
+    name = "";
+    price = 0.01;
+    id = 0L;
+
     }
 
     public Item(Long id, String name, Double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public Long getId() {
-        return null;
+        return id;
     }
 
     public void setId(Long id) {
+        this.id = id;
     }
 
     public Double getPrice() {
-        return null;
+        return price;
     }
 
     public void setPrice(Double price) {
+        this.price = price;
     }
 }
